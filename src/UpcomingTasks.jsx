eb376969f764945cbc00a22fc99eb27479bc0671
Jigyasa Kaushik <jkaushik0802@gmail.com>
2024-05-14 20:07:36 +0530

    Created Upcoming Tasks component

--- src/UpcomingTasks.jsx
+++ src/UpcomingTasks.jsx
@@ -1,111 +1,45 @@
-const UpcomingTasks = ({ tasks }) => {
-  const today = new Date();
-  const todayDate = today.toISOString().split("T")[0];
+import ListItem from "./ListItem";
 
-  const tomorrow = new Date(today);
-  tomorrow.setDate(today.getDate() + 1);
-  const tomorrowDate = tomorrow.toISOString().split("T")[0];
-
-  const todayTasks = tasks.filter((task) => {
-    const taskDate = new Date(task.dueDate).toISOString().split("T")[0];
-    return taskDate === todayDate && !task.completed;
-  });
-
-  const tomorrowTasks = tasks.filter((task) => {
-    const taskDate = new Date(task.dueDate).toISOString().split("T")[0];
-    return taskDate === tomorrowDate && !task.completed;
-  });
-
+const UpcomingTasks = ({
+  tasks,
+  deleteTask,
+  completeTask,
+  editTask,
+  onDateChange,
+  selectedDate,
+}) => {
   return (
-    <div>
-      {todayTasks.length > 0 && (
-        <div>
-          <h2>Today</h2>
-          {todayTasks.map((task) => (
-            <div key={task.id}>
-              <p>{task.task}</p>
-              <p>Due Date: {task.dueDate}</p>
-            </div>
-          ))}
-        </div>
-      )}
-
-      {tomorrowTasks.length > 0 && (
-        <div>
-          <h2>Tomorrow</h2>
-          {tomorrowTasks.map((task) => (
-            <div key={task.id}>
-              <p>{task.task}</p>
-              <p>Due Date: {task.dueDate}</p>
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
+    <>
+      <div>
+        <h2>Today</h2>
+        {tasks.today.map((task) => (
+          <ListItem
+            key={task.id}
+            task={task}
+            deleteTask={deleteTask}
+            onCompleteTask={completeTask}
+            onTaskChange={editTask}
+            selectedDate={selectedDate}
+            onDateChange={onDateChange}
+          />
+        ))}
+      </div>
+      <div>
+        <h2>Tomorrow</h2>
+        {tasks.tomorrow.map((task) => (
+          <ListItem
+            key={task.id}
+            task={task}
+            deleteTask={deleteTask}
+            onCompleteTask={completeTask}
+            onTaskChange={editTask}
+            selectedDate={selectedDate}
+            onDateChange={onDateChange}
+          />
+        ))}
+      </div>
+    </>
   );
 };
 
 export default UpcomingTasks;
-
-// filterUpcomingTasks(tasks) {
-//     if (!tasks || !tasks.length) return [];
-
-//     const today = new Date();
-//     const tomorrow = new Date(today);
-//     tomorrow.setDate(today.getDate() + 1);
-
-//     const upcomingTasks = tasks.filter((taskItem) => {
-//       const taskDate = new Date(taskItem.dueDate);
-//       return taskDate >= today && taskDate < tomorrow;
-//     });
-
-//     const sortedUpcomingTasks = this.sortTasksByPriority(upcomingTasks);
-
-//     return sortedUpcomingTasks;
-//   }
-
-//   sortTasksByPriority(tasks) {
-//     const sortedTasks = tasks.sort(
-//       (a, b) => this.priorityLevel(a) - this.priorityLevel(b)
-//     );
-//     return sortedTasks;
-//   }
-
-//   priorityLevel(task) {
-//     const today = new Date();
-//     const tomorrow = new Date(today);
-//     tomorrow.setDate(today.getDate() + 1);
-
-//     if (task.dueDate === today.toISOString().split("T")[0]) {
-//       return 0;
-//     } else if (task.dueDate === tomorrow.toISOString().split("T")[0]) {
-//       return 1;
-//     } else {
-//       return 2;
-//     }
-//   }
-
-// sortTasksByPriority(tasks) {
-//     // Sort tasks based on priority level
-//     const sortedTasks = tasks.sort(
-//       (a, b) => this.priorityLevel(a) - this.priorityLevel(b)
-//     );
-//     return sortedTasks;
-//   }
-
-//   priorityLevel(task) {
-//     const today = new Date();
-//     const tomorrow = new Date(today);
-//     tomorrow.setDate(today.getDate() + 1);
-
-//     if (task.dueDate === today.toISOString().split("T")[0]) {
-//       return 0; // Highest priority for tasks due today
-//     } else if (task.dueDate === tomorrow.toISOString().split("T")[0]) {
-//       return 1; // Second priority for tasks due tomorrow
-//     } else {
-//       return 2; // Lower priority for tasks due later
-//     }
-//   }
-
-// const storage = new Storage();
-// export default storage;
