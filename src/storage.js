3457b96829c528f82467256f8456b4641f91b1b5
Jigyasa Kaushik <jkaushik0802@gmail.com>
2024-05-16 01:40:35 +0530

    Todo task completed

--- src/storage.js
+++ src/storage.js
@@ -6,6 +6,25 @@ const FILTER = {
 
 class Storage {
   key = "todoTasks";
+
+  formatDate(date) {
+    // Get day, month, and year
+    var day = date.getDate();
+    var month = date.getMonth() + 1; // Months are zero based
+    var year = date.getFullYear();
+
+    // Pad day and month with leading zeros if needed
+    if (day < 10) {
+      day = "0" + day;
+    }
+    if (month < 10) {
+      month = "0" + month;
+    }
+
+    // Return formatted date
+    return day + "-" + month + "-" + year;
+  }
+
   parse(data) {
     try {
       return JSON.parse(data);
@@ -50,17 +69,17 @@ class Storage {
     if (!tasks || !tasks.length) return [];
 
     const today = new Date();
-    const todayDate = today.toISOString().split("T")[0];
+    const todayDate = this.formatDate(today);
 
     const tomorrow = new Date(today);
     tomorrow.setDate(today.getDate() + 1);
-    const tomorrowDate = tomorrow.toISOString().split("T")[0];
+    const tomorrowDate = this.formatDate(tomorrow);
 
     const todayTasks = [];
     const tomorrowTasks = [];
 
     tasks.forEach((task) => {
-      const taskDueDate = new Date(task.dueDate).toISOString().split("T")[0];
+      const taskDueDate = this.formatDate(new Date(task.dueDate));
       if (!task.completed && taskDueDate === todayDate) {
         todayTasks.push(task);
       } else if (!task.completed && taskDueDate === tomorrowDate) {
