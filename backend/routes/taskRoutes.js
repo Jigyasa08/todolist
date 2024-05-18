bd6415b40459ccb42fe8c7b5972638e94247f415
Jigyasa Kaushik <jkaushik0802@gmail.com>
2024-05-19 01:45:05 +0530

    GET api created

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -3,5 +3,24 @@ const router = express.Router();
 const Task = require("../models/task");
 
 // Define routes
+router.get("/", async (req, res, next) => {
+  const taskList = await Task.find({});
+  res.json(taskList);
+});
+
+router.post("/", (req, res, next) => {
+  // TODO...
+  // get task object and create a new record for task and return new task object
+});
+
+router.patch("/:taskId", (req, res, next) => {
+  // TODO...
+  // find by task and update task and return new task object
+});
+
+router.delete("/:taskId", (req, res, next) => {
+  // TODO...
+  // delete mongo and return status
+});
 
 module.exports = router;
