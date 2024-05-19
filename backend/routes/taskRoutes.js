509c19bccc88a84f8b7d4a4e28917359a45500a5
Jigyasa Kaushik <jkaushik0802@gmail.com>
2024-05-20 00:59:53 +0530

    Joi validator installed and used for validation in api and delete method created

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const Task = require("../models/task");
+const Joi = require("joi");
+
+const validationSchema = Joi.object().keys({
+  title: Joi.string().required(),
+  dueDate: Joi.date().iso(),
+  isCompleted: Joi.boolean().sensitive(),
+});
 
 // Define routes
 router.get("/", async (req, res) => {
@@ -9,20 +16,31 @@ router.get("/", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-  // get task object and create a new record for task and return new task object
-  const newTaskData = req.body;
-  // ADD VALIDATION
+  // VALIDATION
+  const { error, value } = validationSchema.validate(req.body);
+  if (error) {
+    return res.status(422).json({
+      error: error,
+    });
+  }
+  const newTaskData = value;
   // Save to mongo
   const task = new Task({ ...newTaskData, isCompleted: false });
   await task.save();
   res.json(task);
 });
 
-router.patch("/:taskId", async (req, res, next) => {
+router.patch("/:taskId", async (req, res) => {
   // TODO...// ADD VALIDATION
   // find by task and update task and return new task object
   const taskId = req.params.taskId;
-  const updatedTaskData = req.body;
+  const { error, value } = validationSchema.validate(req.body);
+  if (error) {
+    return res.status(422).json({
+      error: error,
+    });
+  }
+  const updatedTaskData = value;
   const updatedTask = await Task.findById(taskId);
 
   if (!updatedTask) {
@@ -30,11 +48,7 @@ router.patch("/:taskId", async (req, res, next) => {
     return res.status(400).json({ error: "Error! Invalid task Id" });
   }
 
-  if (updatedTaskData && updatedTaskData.title) {
-    updatedTask.title = updatedTaskData.title;
-  }
-
-  if (updatedTaskData && updatedTaskData.title) {
+  if (updatedTaskData?.title) {
     updatedTask.title = updatedTaskData.title;
   }
 
@@ -46,17 +60,26 @@ router.patch("/:taskId", async (req, res, next) => {
     if (!updatedTask.isCompleted && updatedTaskData.isCompleted) {
       // Update the completed date
       updatedTask.completedDate = new Date();
+    } else {
+      updatedTask.completedDate = undefined;
     }
     updatedTask.isCompleted = updatedTaskData.isCompleted;
   }
+
+  updatedTask.updatedDate = Date.now();
 
   await updatedTask.save();
   res.json(updatedTask);
 });
 
-router.delete("/:taskId", (req, res, next) => {
+router.delete("/:taskId", async (req, res) => {
   // TODO...
+  const taskId = req.params.taskId;
   // delete mongo and return status
+  await Task.findByIdAndDelete(taskId);
+  return res.json({
+    status: "Success",
+  });
 });
 
 module.exports = router;
